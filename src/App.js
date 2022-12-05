1ea9b13b10a76b5ef823b61b91132576d6f501e6
IvanSantibanez <ivan.t.asc@gmail.com>
2022-12-05 17:15:30 -0300

    Proyecto autitos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Subaru_WRX_STI_2022 from "./img/Subaru_WRX_STI_2022.jpg"
+import hyundaigrandi10 from "./img/hyundaigrandi10.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App= ()=>{
+    return(
+        <div align="center">
+            <h1> Esto es un proyecto de autitos</h1>
+            <h2>  Vamos como avión</h2>
+            <table border="1">
+                <tr> 
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Año</th>
+                    <th>Precio</th>
+                </tr>
+                <tr>
+                    <td>Subaru</td>
+                    <td>Impreza WRX Sti</td>
+                    <td>2022</td>
+                    <td>25.000.000</td>
+                </tr>
+            </table>
+            <img src={Subaru_WRX_STI_2022} alt="subaruImp"/>
+            <div align="center">
+            <table border="1">
+                <tr> 
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Año</th>
+                    <th>Precio</th>
+                </tr>
+                <tr>
+                    <td>Hyundai</td>
+                    <td>Grand i10</td>
+                    <td>2022</td>
+                    <td>9.000.000</td>
+                </tr>
+            </table>
+            <img src={hyundaigrandi10} alt="HyundaiGi10"/>
+
+
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
